14ceacb10475b7317bb595612af1298f9540e65f
Rosver Jedh <122253642+Rosver18@users.noreply.github.com>
2025-06-03 22:01:54 +0400

    Update server.js

--- server.js
+++ server.js
@@ -4,28 +4,27 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public')); // Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Email Route
 app.post('/send-email', async (req, res) => {
   const { name, email, message } = req.body;
 
-  // Create transporter using Gmail
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'rosver5439@gmail.com',     
-      pass: 'tzdr gmbe muwa ezrj'         
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
     }
   });
 
   const mailOptions = {
     from: email,
-    to: 'rosver5439@gmail.com', 
+    to: process.env.EMAIL_TO,
     subject: `Message from ${name}`,
     text: `
       Name: ${name}
@@ -36,14 +35,13 @@ app.post('/send-email', async (req, res) => {
 
   try {
     await transporter.sendMail(mailOptions);
-    res.redirect('/thankyou.html'); 
+    res.redirect('/thankyou.html');
   } catch (error) {
     console.error('Email sending failed:', error);
     res.status(500).send('An error occurred while sending the email.');
   }
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
